345ea0bfa5258f9f0b6a4dd5b7f84b0698ce273b
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-22 06:56:26 -0300

    Primeiro commit

---
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/ApiSpringBootWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.paulorjuniorp.apispringbootwithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulorjuniorp/apispringbootwithmongodb/ApiSpringBootWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulorjuniorp.apispringbootwithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
