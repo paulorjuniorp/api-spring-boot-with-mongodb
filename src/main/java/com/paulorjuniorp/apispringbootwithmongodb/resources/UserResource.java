4d032f43ab24c24f9e3fac8bb5c14f37b58682ba
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-25 07:31:46 -0300

    Refatoração do controller para utilizar o service e adição da string de configuração de conexão do mongodb

--- src/main/java/com/paulorjuniorp/apispringbootwithmongodb/resources/UserResource.java
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/resources/UserResource.java
@@ -1,22 +1,25 @@
 package com.paulorjuniorp.apispringbootwithmongodb.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paulorjuniorp.apispringbootwithmongodb.domain.User;
+import com.paulorjuniorp.apispringbootwithmongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		List<User> list = new ArrayList<>();
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(userService.findAll());
 	}
 }
