803ef177f312475267099b3b846850c75fae0e4f
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-25 06:56:59 -0300

    Criação do UserRepository extendendo MongoRepository

---
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.paulorjuniorp.apispringbootwithmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.paulorjuniorp.apispringbootwithmongodb.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
