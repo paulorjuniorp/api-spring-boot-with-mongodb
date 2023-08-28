e46ae8e65fdc6fc98c5ad9084aa77212c9958318
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-28 06:51:52 -0300

    Método insert no controller de User

--- src/main/java/com/paulorjuniorp/apispringbootwithmongodb/resources/UserResource.java
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.paulorjuniorp.apispringbootwithmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.paulorjuniorp.apispringbootwithmongodb.domain.User;
 import com.paulorjuniorp.apispringbootwithmongodb.dto.UserDTO;
@@ -36,5 +40,13 @@ public class UserResource {
 		UserDTO userDTO = new UserDTO(user);
 		
 		return ResponseEntity.ok().body(userDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+		User entity = userService.fromDTO(objDTO);
+		entity = userService.insert(entity);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(entity.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
