d7e44a488dbb0990eb1c613c8954d52ea5419943
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-29 20:35:37 -0300

    Adição das anotações de mapeamento

--- src/main/java/com/paulorjuniorp/apispringbootwithmongodb/domain/Post.java
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/domain/Post.java
@@ -4,10 +4,15 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Post implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private Date date;
 	private String title;
