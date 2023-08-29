2becb6f397457354f7c47d417a43122f101aa1c1
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-29 20:46:19 -0300

    Injeção de exemplos na classe de inicialização

--- src/main/java/com/paulorjuniorp/apispringbootwithmongodb/config/Instantiation.java
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.paulorjuniorp.apispringbootwithmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.paulorjuniorp.apispringbootwithmongodb.domain.Post;
 import com.paulorjuniorp.apispringbootwithmongodb.domain.User;
+import com.paulorjuniorp.apispringbootwithmongodb.repository.PostRepository;
 import com.paulorjuniorp.apispringbootwithmongodb.repository.UserRepository;
 
 @Configuration
@@ -14,9 +18,15 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+		formatoData.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -25,6 +35,10 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post post1 = new Post(null, formatoData.parse("21/12/2023"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, formatoData.parse("26/09/2023"), "Bom dia", "Acordei feliz hoje!",maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
