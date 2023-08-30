990766d3f3d62271ae7b3a20245ee959795e3d6e
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-30 06:54:18 -0300

    Criação do AuthorDTO e alteração do Post para receber o Author ao invés do User

--- src/main/java/com/paulorjuniorp/apispringbootwithmongodb/config/Instantiation.java
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.paulorjuniorp.apispringbootwithmongodb.domain.Post;
 import com.paulorjuniorp.apispringbootwithmongodb.domain.User;
+import com.paulorjuniorp.apispringbootwithmongodb.dto.AuthorDTO;
 import com.paulorjuniorp.apispringbootwithmongodb.repository.PostRepository;
 import com.paulorjuniorp.apispringbootwithmongodb.repository.UserRepository;
 
@@ -35,8 +36,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, formatoData.parse("21/12/2023"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, formatoData.parse("26/09/2023"), "Bom dia", "Acordei feliz hoje!",maria);
+		Post post1 = new Post(null, formatoData.parse("21/12/2023"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, formatoData.parse("26/09/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
