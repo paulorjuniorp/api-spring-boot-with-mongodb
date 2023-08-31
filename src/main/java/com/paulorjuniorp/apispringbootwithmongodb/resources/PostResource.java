528c273277a968d4f322daf4175109e79a8951ed
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-31 07:07:25 -0300

    Criação do controller de Post

--- src/main/java/com/paulorjuniorp/apispringbootwithmongodb/resources/PostResource.java
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/resources/PostResource.java
@@ -1,5 +1,25 @@
 package com.paulorjuniorp.apispringbootwithmongodb.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paulorjuniorp.apispringbootwithmongodb.domain.Post;
+import com.paulorjuniorp.apispringbootwithmongodb.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
 public class PostResource {
 
+	@Autowired
+	private PostService postService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post post = postService.findById(id);		
+		return ResponseEntity.ok().body(post);
+	}
 }
