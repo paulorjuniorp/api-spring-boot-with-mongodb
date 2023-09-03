b96f686147b0237da3948247c340161c30d6067b
paulorjuniorp <paulorjuniorp@gmail.com>
2023-09-02 22:01:06 -0300

    Adição do método fullSearch no PostService

--- src/main/java/com/paulorjuniorp/apispringbootwithmongodb/services/PostService.java
+++ src/main/java/com/paulorjuniorp/apispringbootwithmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.paulorjuniorp.apispringbootwithmongodb.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +31,10 @@ public class PostService {
 	
 	public List<Post> searchTitle(String text){
 		return postRepository.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 }
